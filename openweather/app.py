20a8ead346b33153ea8a82cbd7779393ae5253e0
Juan Gallostra <juangallostra@gmail.com>
2020-07-31 18:55:13 +0200

    Generate widget from path

--- openweather/app.py
+++ openweather/app.py
@@ -1,9 +1,18 @@
 from flask import Flask, render_template
+import json
 app = Flask(__name__)
 
-@app.route('/')
-def widget():
-    return render_template('widget.html')
+@app.route('/<string:zone>')
+def widget(zone):
+    datafile = 'data/zones/' + zone + '/' + zone + '.txt'
+    area_data = {}
+    with open(datafile, encoding='utf-8') as data:
+        area_data = json.load(data)
+    lat = area_data['latitude']
+    lng = area_data['longitude']
+    name = area_data['name']
+
+    return render_template('widget.html', lat=lat, lng=lng, zone=name, lang='es')
 
 # start the server
 if __name__ == '__main__':
